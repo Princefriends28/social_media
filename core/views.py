74691d62a14e9a95fdbb1c5ef9968f612d099e72
Princefriends28 <princefriends28@gmail.com>
2022-12-20 22:17:15 +0530

    Upload Post WIP

--- core/views.py
+++ core/views.py
@@ -8,7 +8,10 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    return render(request, 'index.html', {user_profile: 'user_profile'})
 
 
 @login_required(login_url='signin')
@@ -99,3 +102,7 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
